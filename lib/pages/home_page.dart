bd960af438da223a5d3c659ff9a60218f6c66534
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-12 22:52:55 -0500

    operaciones

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,16 +16,73 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-                onPressed: () {
-                  usersReference.get().then((value) {
+              onPressed: () {
+                usersReference.get().then((value) {
+                  QuerySnapshot userCollection = value;
+                  List<QueryDocumentSnapshot> docs = userCollection.docs;
+                  docs.forEach((element) {
+                    print(element.id);
+                    print(element.data());
+                  });
+                });
+              },
+              child: Text("Traer Data"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference
+                    .where("nombre", isEqualTo: "Pedro")
+                    // .where("estatura", isGreaterThan: 175)
+                    .get()
+                    .then(
+                  (value) {
                     QuerySnapshot userCollection = value;
                     List<QueryDocumentSnapshot> docs = userCollection.docs;
-                    docs.forEach((element) {
-                      print(element.id);
+                    docs.forEach((e) {
+                      print(e.id);
+                      print(e.data());
                     });
-                  });
-                },
-                child: Text("Traer Data"))
+                  },
+                );
+              },
+              child: Text("Traer Data FILTRADA"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.add({
+                  'nombre': "Pedro",
+                  "apellido": "Fernandez",
+                  "estatura": 178,
+                  "esPeruano": true,
+                }).then((value) {
+                  print(value.id);
+                });
+              },
+              child: Text("Agregar usuario"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.doc("idNoExiste").set(
+                  {
+                    'nombre': "Javier",
+                    "apellido": "Mader",
+                    "estatura": 198,
+                    "esPeruano": false,
+                  },
+                );
+              },
+              child: Text("Inserción 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.doc("idNoExiste").update(
+                  {
+                    "nombre": "Anastacio",
+                  },
+                );
+              },
+              child: Text("Actualización"),
+            )
           ],
         ),
       ),
